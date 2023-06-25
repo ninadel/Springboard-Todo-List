da29015d677729f5f532e930bc7dfd74ed143523
Nina del Rosario <nina.del@gmail.com>
2023-06-25 09:17:52 -0700

    cleaning up console logs

--- app.js
+++ app.js
@@ -18,25 +18,18 @@ document.addEventListener("DOMContentLoaded", function () {
     localStorage.setItem("storedList", null);
     let todoStorage = [];
     let todoItems = document.querySelectorAll("li");
-    console.log("todoItems", todoItems);
-    console.log(todoItems.length);
 
     if (todoItems.length > 0) {
       for (i = 0; i < todoItems.length; i++) {
         let item = todoItems[i];
         let itemText = null;
         itemText = item.innerText.slice(0, item.innerText.length - 1);
-        console.log("itemText", itemText);
-        console.log("item classlist", item.classList);
         if (item.classList.contains("incomplete")) {
           todoStorage.push([itemText, "incomplete"]);
-          console.log("storing incomplete");
         } else if (item.classList.contains("complete")) {
           todoStorage.push([itemText, "complete"]);
-          console.log("storing complete");
         }
         localStorage.setItem("storedList", JSON.stringify(todoStorage));
-        console.log("localStorage", localStorage);
       }
     }
   }
@@ -46,19 +39,14 @@ document.addEventListener("DOMContentLoaded", function () {
     let listItemTaskValue = null;
     let listItemState = null;
     if (storedList !== null) {
-      console.log("recallTodoList");
-      console.log("storedList", storedList);
       for (i = 0; i < storedList.length; i++) {
         listItem = storedList[i];
         listItemTaskValue = listItem[0];
-        console.log("listItemTaskValue", listItemTaskValue);
         listItemState = listItem[1];
         addListItem(listItemTaskValue, listItemState, false);
       }
     } else {
-      console.log("skipping recall");
     }
-    console.log("storing after recall");
     storeTodoList();
   }
 
@@ -81,7 +69,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     todoItem.addEventListener("click", function (e) {
       let targetTag = e.target.tagName;
-      console.log(targetTag);
       if (targetTag === "BUTTON") {
         e.target.parentNode.remove();
       } else if (targetTag === "LI") {
@@ -89,15 +76,10 @@ document.addEventListener("DOMContentLoaded", function () {
           e.target.style.textDecoration = "line-through";
           e.target.classList.add("complete");
           e.target.classList.remove("incomplete");
-          console.log(
-            "classList after completing incomplete",
-            e.target.classList
-          );
         } else if (todoItem.classList.contains("complete")) {
           e.target.style.textDecoration = "none";
           e.target.classList.add("incomplete");
           e.target.classList.remove("complete");
-          console.log("classList after toggling complete", e.target.classList);
         }
       }
       storeTodoList();
